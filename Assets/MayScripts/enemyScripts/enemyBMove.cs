1d02af23c597c550a8e52bca34e42ada4c342de6
Lilinamay <rc4333@nyu.edu>
2021-11-30 03:03:07 -0500

    enemyB complete

--- Assets/MayScripts/enemyScripts/enemyBMove.cs
+++ Assets/MayScripts/enemyScripts/enemyBMove.cs
@@ -11,7 +11,17 @@ public class enemyBMove : MonoBehaviour
     private float enemyX;
     Rigidbody2D enemyBody;
     SpriteRenderer myRenderer;
+    public Animator EnemyBAnimator;
 
+    public float RayDis = 10f;
+    public bool alert = false;
+    public float dashMaxDis = 6;
+    float dashleftbound;
+    float dashrightbound;
+    public float orgPosx;
+    //public float Posx;
+    public float dashCD = 0;
+    public float chargeCD = 0;
     ///[SerializeField] private Sprite[] IdleSprites;
 
     //[SerializeField] private float animationSpeed = 0.3f;
@@ -24,6 +34,10 @@ public class enemyBMove : MonoBehaviour
         myRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         enemyX = gameObject.transform.position.x;
+
+        //orgPosx = transform.position.x;
+        dashleftbound = enemyX - (dashMaxDis / 2);
+        dashrightbound = enemyX + (dashMaxDis / 2);
     }
 
     // Update is called once per frame
@@ -52,19 +66,112 @@ public class enemyBMove : MonoBehaviour
             myRenderer.flipX = true;
         }
 
-        enemyBody.velocity = new Vector3(enemySpeed, enemyBody.velocity.y);
+       
+
+        //Posx = transform.position.x;
+        if (alert)
+        {
+            if (enemyCurrentX >= dashleftbound && enemyCurrentX <= dashrightbound)
+            {
+                if (dashCD <= 0)
+                {
+                    dashCD = 5;
+                    //chargeCD = 1;
+                    enemyBody.velocity = new Vector2(0, 0);
+                    Debug.Log("CHARGE");
+                    EnemyBAnimator.SetBool("canCharge", true);
+                }
+            }
+            else
+            {
+                //walk
+                enemyBody.velocity = new Vector2(enemySpeed, 0);
+            }
+        }
+        else
+        {
+            //walk
+            enemyBody.velocity = new Vector2(enemySpeed, 0);
+        }
+
+        if (dashCD <= 4 && dashCD >= 2)
+        {
+            //DASH
+            enemyBody.velocity = new Vector2(enemySpeed * 2, 0);
+            EnemyBAnimator.SetBool("canCharge", false);
+            EnemyBAnimator.SetBool("canAttack", true);
+        }
+        else if (dashCD < 2 && dashCD >= 1)
+        {
+            enemyBody.velocity = new Vector2(0, 0);
+            //stop dash
+        }
+        else if (dashCD < 1)
+        {
+            //enemyBody.velocity = new Vector3(enemySpeed, enemyBody.velocity.y);
+            enemyBody.velocity = new Vector2(enemySpeed, 0);
+            EnemyBAnimator.SetBool("canAttack", false);
+        }
+
+
+        if (dashCD >= 0)
+        {
+            dashCD -= Time.deltaTime;
+        }
+        if (chargeCD >= 0)
+        {
+           chargeCD -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!myRenderer.flipX)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, RayDis, 7);
+            if (hit.collider)
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.name == "Player")
+                {
+                    //attack ready
+                    alert = true;
+                }
+                else
+                {
+                    alert = false;
+                }
+            }
+            else
+            {
+                alert = false;
+            }
+        }
+        if (myRenderer.flipX)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, RayDis, 7);
+            if (hit.collider)
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.name == "Player")
+                {
+                    //attack ready
+                    alert = true;
+                }
+                else
+                {
+                    alert = false;
+                }
+            }
+            else
+            {
+                alert = false;
+            }
+        }
+        //Debug.DrawRay(hit);
 
     }
 
-    /*void MovingAnimation(Sprite[] currentSprite)                                    //animations
-    {
-        timer += Time.deltaTime;
-        if (timer >= animationSpeed)
-        {
-            timer = 0;
-            currentSpriteIndex++;
-            currentSpriteIndex %= currentSprite.Length;
-        }
-        myRenderer.sprite = currentSprite[currentSpriteIndex];
-    }*/
+
+
 }
